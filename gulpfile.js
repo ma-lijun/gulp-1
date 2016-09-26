b6df2a304cbab3718dd8863c52d4c1174cbb7938
doveaz <383693011@qq.com>
2016-09-26 17:36:52 +0800

    初步完成

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,4 @@
-/*** Created by Doveaz on 2016/9/24.*/
-
+/*** Created by Doveaz on 2016/9/24.  https://github.com/DoveAz/gulp5811 */
 
 var root = "test/",                //项目目录
 
@@ -9,16 +8,29 @@ var root = "test/",                //项目目录
 
     serverRoot = dist,             //服务器根目录
     serverPort = '80',             //服务器端口
+    serverIndex = "noindex.html",    //服务器默认打开页面（为了显示页面列表，设置noindex.html）
     serverHost = 'localhost';      //服务器地址
+
+
+var imagemin = false,              //是否开启图片压缩
+    sprite = false,               //是否合并雪碧图
+    cssminify = true;             //css压缩
 
 //生产目录文件夹
 var distDir = {
     css: dist + 'css',
-    images: dist + 'images'
+    images: dist + 'images',
+    js: dist + 'js'
+};
+
+var doyoDir = {
+    css: doyo + 'css',
+    images: doyo + 'images',
+    js: doyo + 'js'
 };
 
 //静态文件目录
-var assetDir = {
+var asset = {
     html: www,
     js: www + 'js/',
     css: www + 'css/',
@@ -27,21 +39,27 @@ var assetDir = {
 };
 
 //静态文件
-var asset = {
-    html: [assetDir.html + '*.html', assetDir.html + 'template/*.html'],
-    js: assetDir.js + '/*.js',
-    css: assetDir.css + '*.css',
-    less: assetDir.less + '**/*.less',
-    images: assetDir.images + "**/*.{png,jpg,gif}"
+var assetGlobs = {
+    html: [asset.html + '*.html', asset.html + 'template/*.html'],
+    js: asset.js + '/*.js',
+    css: asset.css + '*.css',
+    less: asset.less + '**/*.less',
+    images: asset.images + "**/*.{png,jpg,gif}"
 };
 
-
+// 模块调用
 var gulp = require('gulp'),
     gulpif = require('gulp-if'),
+    cssnano = require('gulp-cssnano'),
+    gcmq = require('gulp-group-css-media-queries'),
+    del = require('del'),
+    minify = require('gulp-minify'),
     ejs = require('gulp-ejs'),
     dest = require('gulp-dest'),
     connect = require('gulp-connect'),
     less = require('gulp-less'),
+    replace = require('gulp-replace'),
+    imagemin = require('gulp-imagemin'),
     autoprefixer = require('gulp-autoprefixer'),
     postcss = require('gulp-postcss'),
     spriter = require('gulp-css-spriter'),
@@ -51,13 +69,13 @@ var gulp = require('gulp'),
     cssgrace = require('cssgrace');
 
 //默认任务
-gulp.task('default', ['watch', 'ejs', 'less', 'css', 'connect']);
+gulp.task('default', ['watch', 'ejs', 'less', 'css', 'images', 'connect']);
 
-//发布 会清理html和css模板文件
-gulp.task('dist', ['default', 'clean']);
+//发布 会清理html和css模板文件,并且压缩css，js
+gulp.task('dist', ['ejs', 'less', 'css', 'images', 'cleanCss', 'cleanHtml', 'minify']);
 
-//发布适合doyo cms使用的文件 会替换include标签 并清理 css模板文件
-gulp.task('doyo', ['default']);
+//发布适合doyo cms使用的文件 会替换include标签 并清理css模板文件
+gulp.task('doyo', ['doyo-build']);
 
 //服务器配置
 gulp.task('connect', function () {
@@ -65,20 +83,22 @@ gulp.task('connect', function () {
         port: serverPort,
         host: serverHost,
         root: serverRoot,
-        livereload: true
+        livereload: true,
+        index: serverIndex
     })
 });
 
 //监视文件
 gulp.task('watch', function () {
-    gulp.watch(asset.html, ['ejs']);
-    gulp.watch(asset.less, ['less']);
-    gulp.watch(asset.css, ['css'])
+    gulp.watch(assetGlobs.html, ['ejs']);
+    gulp.watch(assetGlobs.less, ['less']);
+    gulp.watch(assetGlobs.css, ['css']);
+    gulp.watch(assetGlobs.images, ['images'])
 });
 
 //编译ejs
 gulp.task('ejs', function () {
-    gulp.src(asset.html, {base: assetDir.html})
+    return gulp.src(assetGlobs.html, {base: asset.html})
         .pipe(plumber({errorHandler: notify.onError("ejs编译失败，请检查代码")}))
         .pipe(ejs({
             msg: "Hello Gulp!"
@@ -92,11 +112,11 @@ gulp.task('less', function () {
     var processors = [
         require('cssgrace')
     ];
-    gulp.src(asset.less, {base: assetDir.less})
+    return gulp.src(assetGlobs.less, {base: asset.less})
         .pipe(plumber({errorHandler: notify.onError("less编译失败，请检查代码")}))
         .pipe(less())
         .pipe(autoprefixer({
-            browsers: ['last 5 versions', 'IE 6-8', '>2%']
+            browsers: ['last 5 versions', 'IE 6-8', '>1%']
         }))
         .pipe(postcss(processors))
         .pipe(gulp.dest(distDir.css))
@@ -105,14 +125,82 @@ gulp.task('less', function () {
 
 //发布静态文件到dist目录
 gulp.task('images', function () {
-    gulp.src(asset.images)
+    return gulp.src(assetGlobs.images)
         .pipe(gulp.dest(distDir.images))
 
 });
 gulp.task('css', function () {
-    gulp.watch(asset.css, function () {
-        gulp.src(asset.css)
-            .pipe(gulp.dest(distDir.css))
-            .pipe(connect.reload())
-    })
+    return gulp.src(assetGlobs.css)
+        .pipe(gulp.dest(distDir.css))
+        .pipe(connect.reload())
 });
+
+//清理template文件
+gulp.task('cleanHtml', function () {
+    del([
+        dist + 'template',
+        distDir.css + '/template'
+    ])
+});
+
+//压缩css，js，图片
+gulp.task('minify', ['jsminify', 'cssnano', 'imagemin']);
+
+gulp.task('jsminify', function () {
+    return gulp.src(distDir.js)
+        .pipe(minify({
+            ext: {
+                min: '.js'
+            },
+            ignoreFiles: ['-min.js']
+        }))
+        .pipe(gulp.dest(distDir.js))
+});
+gulp.task('cssnano', function () {
+    return gulp.src(distDir.css + '/*.css')
+        .pipe(gcmq())
+        .pipe(gulpif(cssminify, cssnano({
+            discardComments: {
+                removeAll: true
+            },
+            discardOverridden: true,
+            mergeLonghand: true,
+            minifyFontValues: true,
+            mergeRules: true,
+            minifySelectors: true,
+            mergeIdents: true
+        })))
+        .pipe(gulp.dest(distDir.css))
+});
+gulp.task('imagemin', function () {
+    return gulp.src(distDir.images + '/*')
+        .pipe(gulpif(imagemin, imagemin()))
+        .pipe(gulp.dest(distDir.images))
+});
+
+gulp.task('doyo-build', function () {
+    var processors = [
+        require('cssgrace')
+    ];
+    gulp.src(assetGlobs.less, {base: asset.less})
+        .pipe(plumber({errorHandler: notify.onError("less编译失败，请检查代码")}))
+        .pipe(less())
+        .pipe(autoprefixer({
+            browsers: ['last 5 versions', 'IE 6-8', '>2%']
+        }))
+        .pipe(postcss(processors))
+        .pipe(gulp.dest(doyoDir.css));
+
+    gulp.src(assetGlobs.images)
+    // .pipe(gulpif(imagemin, imagemin()))
+        .pipe(gulp.dest(doyoDir.images));
+
+    gulp.src(assetGlobs.css)
+        .pipe(gulp.dest(doyoDir.css));
+
+    var doyoReg = /\<\%\-\s+include\(\'(.*)\'\)\s+\-?\%\>/g;
+    gulp.src(assetGlobs.html, {base: asset.html})
+        .pipe(replace(doyoReg, '{include = "$1" }'))
+        .pipe(gulp.dest(doyo));
+});
+
--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@
 -
 **1.安装nodejs**
 
-你可以在 [官方网站](https://nodejs.org/en/) 下载，
+你可以在 [官方网站](https://nodejs.org/en/) 下载（下载最新版），
 或者 [点击这里](https://nodejs.org/dist/v6.6.0/node-v6.6.0-x64.msi)下载,
 下载完成之后安装一直选择下一步即可。
 
@@ -41,5 +41,97 @@ npm是nodejs的包管理器，用于安装卸载模块
         
 **4.开始使用**
 
-项目目录结构
+下载本项目至本地，cmd 定位到 本项目根目录。
+
+例：下载到了桌面，鼠标放在项目文件夹上，按住shift点击鼠标右键，选择在此处打开命令窗口
+
+输入
+
+    npm i
+    
+安装完成后打开gulpfile.js文件，修改第三行目录为在做项目的项目目录（建议将项目移动进来），之后在CMD输入
+
+    gulp 
+    
+    
+启动本地服务器（也可以使用处于同一局域网的其他设备打开）
+将会监视HTML、CSS、JS文件变化，自动编译，自动刷新浏览器。
+
+**5.实现的功能**
+
++ HTML 模板引入（ejs模板引擎，具体语法参照 [mde/ejs](https://github.com/mde/ejs)）
+
+        <%- include('template/header.html') %>
+       
++ less 自动编译（语法参照 [less中文网](http://lesscss.cn/)），less是css的超集，因此即使你不想使用less，可以直接在里面写css
+
+less支持引入，因此你可以事先定义一些变量和mixins(类似函数)
+
+    @import "template/var.less";
+
+比如媒体查询的mixins
+      
+     .xs(@rules){
+       @media screen and (max-width:768px){
+         @rules();
+       }
+     }
+        
+然后你就可以在任意地方
+ 
+    body{
+      height: 50px;
+      .xs({ height: 50px;})
+    }
+    
+会自动编译为(最后媒体查询会被gulp-group-css-media-queries插件合并在一起)
+
+    body {
+      height: 50px;
+    }
+    @media screen and (max-width: 768px) {
+      body {
+        height: 50px;
+      }
+    }
+    
+    
++ css，js压缩（cssnano）
++ 自动加浏览器前缀
+
+
+当前设置兼容性为 
+
+    browsers: ['last 5 versions', 'IE 6-8', '>1%']
+    
++ 处理透明度兼容问题,inline-block自动hack
+
+**6.各任务简介**
+
++ 默认任务
+
+        gulp
+ 
++ 最终发布，将会清理html和css模板文件,并且压缩css，js  
+
+        gulp dist
+    
++ doyo发布
+
+         gulp doyo
+
+将源代码编译至doyo文件夹，并且替换 ()
+
+    <%- include('template/header.html') %>
+    
+为
+    
+    {include = "template/header.html" }
+
+    
+
+
+
+
+
 
